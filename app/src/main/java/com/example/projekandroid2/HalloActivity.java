e88585163730b8ca703a7f2f3d227d3ab16614f9
aditya <adityawijaya0304@gmail.com>
2023-11-29 12:43:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projekandroid2/HalloActivity.java
@@ -0,0 +1,14 @@
+package com.example.projekandroid2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HalloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hallo);
+    }
+}
